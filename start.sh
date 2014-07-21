b3c703df39f6047fe245431fa1099efe6534774f
Daniel Craigmile <dcraigmile@texastribune.org>
2014-07-21 13:06:15 -0500

    just use /etc/hosts

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 set -o nounset
 set -o errexit
 set -o pipefail
-# could also use the fact that it's in /etc/hosts?
-sed -e "s/ES_HOST/${ES_PORT_9300_TCP_ADDR}/g" -i /logstash.conf
+
 /opt/logstash/bin/logstash -f /logstash.conf
